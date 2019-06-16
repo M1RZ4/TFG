58fa881980c33900e3d8ec66a94f977112e17b89
Mirza Ojeda Veira <UO251443@uniovi.es>
2019-06-16 15:55:49 +0200

    Completada la planificación manual
    Ya se puede realizar correctamente la planificación seleccionando tareas desde la tabla y se controlan los posibles errores / prácticas no deseadas

--- src/main/java/gui/dialogs/TasksDialog.java
+++ src/main/java/gui/dialogs/TasksDialog.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.DoubleStream;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -19,6 +22,7 @@ import javax.swing.table.JTableHeader;
 
 import org.jfree.chart.ChartPanel;
 
+import dominio.Intervalo;
 import main.java.gui.ApplicationWindow;
 import main.java.logic.LanguageManager;
 import main.java.logic.ScheduledInstance;
@@ -41,11 +45,15 @@ public class TasksDialog extends JDialog {
 	private ApplicationWindow app;
 	private int tickUnit;
 	private ScheduledInstance i;
-	private int sti;
+	private int startTime;
 	private int step = 0;
+	private int[] avaliable;
+	private List<Integer> tasks = new ArrayList<Integer>();
 
 	public TasksDialog(ApplicationWindow app, int tickUnit) {
 		this.app = app;
+		i = new ScheduledInstance(app.getManager().getInstance());
+		fillAvaliableCapacity();
 		this.tickUnit = tickUnit;
 		getContentPane().setBackground(Color.WHITE);
 		setTitle(LanguageManager.getInstance().getTexts().getString("table_tasks"));
@@ -55,6 +63,21 @@ public class TasksDialog extends JDialog {
 		getContentPane().add(getScrollPane(), BorderLayout.CENTER);
 		getContentPane().add(getButtonPanel(), BorderLayout.SOUTH);
 		pack();
+	}
+
+	/**
+	 * Método auxiliar que genera un vector con las capacidades disponibles en cada
+	 * momento
+	 */
+	private void fillAvaliableCapacity() {
+		avaliable = new int[(int) DoubleStream.of(app.getManager().getP()).sum()];
+		List<Intervalo> list = i.getPerfilMaquina();
+		int sum = (int) DoubleStream.of(app.getManager().getP()).sum();
+		for (int j = 0; j < sum; j++) {
+			for (Intervalo in : list)
+				if (j >= in.getInicio() && j < in.getFin())
+					avaliable[j] = in.getCap();
+		}
 	}
 
 	private JScrollPane getScrollPane() {
@@ -121,27 +144,65 @@ public class TasksDialog extends JDialog {
 		return btnPlanificar;
 	}
 
+	/**
+	 * Método auxiliar
+	 */
 	private void scheduleTask() {
 		int row = tasksTable.getSelectedRow();
 		if (row == -1)
 			JOptionPane.showMessageDialog(null, LanguageManager.getInstance().getTexts().getString("error_task_table"),
 					LanguageManager.getInstance().getTexts().getString("error_title"), JOptionPane.ERROR_MESSAGE);
-		else {
-			i = new ScheduledInstance(app.getManager().getInstance());
-			i.setID((int) tasksTable.getValueAt(row, 0));
-			i.setD((double) tasksTable.getValueAt(row, 1));
-			i.setP((double) tasksTable.getValueAt(row, 2));
-			i.setSti(sti); // TODO añadir el valor de sti a la tabla igual que en application window
-			sti += (double) tasksTable.getValueAt(row, 1);
-			
+		else if (tasks.contains((int) tasksTable.getValueAt(row, 0))) {
+			JOptionPane.showMessageDialog(null, LanguageManager.getInstance().getTexts().getString("error_repeated"),
+					LanguageManager.getInstance().getTexts().getString("error_title"), JOptionPane.ERROR_MESSAGE);
+		} else {
+			double duration = (double) tasksTable.getValueAt(row, 1);
+			boolean less = false;
+			// Calcular menor tiempo de inicio
+			loop: for (int j = 0; j < avaliable.length; j++) {
+				avaliable: if (avaliable[j] > 0 && j < startTime) {
+					for (int k = j; k < j + duration; k++) {
+						if (avaliable[k] == 0)
+							break avaliable;
+					}
+					less = true;
+				}
+				if (less) {
+					startTime = j;
+					break loop;
+				}
+			}
+
+			// Restar capacidad en instantes ocupados
+			int endTime = (int) (startTime + duration);
+			for (int k = startTime; k < endTime; k++)
+				avaliable[k] -= 1;
+
+			int id = (int) tasksTable.getValueAt(row, 0);
+			tasks.add(id);
+			double dueDate = (double) tasksTable.getValueAt(row, 2);
+
+			// Añadir tiempo de inicio a la tabla
+			DefaultTableModel model = (DefaultTableModel) tasksTable.getModel();
+			model.setValueAt(startTime, row, 3);
+
+			// Actualizar parámetros de la instancia
+			i.setID(id);
+			i.setP(duration);
+			i.setD(dueDate);
+			i.setSti(startTime);
+			startTime += duration;
+
+			// Actualizar gráfico principal
 			app.getFrame().remove(app.getCp());
 			app.getManager().setMainChart(step, i, tickUnit);
 			app.setCp(new ChartPanel(app.getManager().getChart()));
 			app.getFrame().getContentPane().add(app.getCp());
 			app.getFrame().revalidate();
-			
+
+			// Incrementar step
 			step++;
-			
+
 		}
 	}
 
